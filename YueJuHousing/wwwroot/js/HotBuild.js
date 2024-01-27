d3aaa2cd0162bc25183ee2a69f43f078659c0103
CHUN-WEI,CHIU <kevin87332000@gmail.com>
2024-01-27 20:46:05 +0800

    (1)首頁熱門物件輪播廣告功能完成 (2)廣告刊登物件頁面建置中(部分完成)

---
+++ YueJuHousing/wwwroot/js/HotBuild.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    // 初始化建案轮播
+    $('.scroll-list.newbuild-hot').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+});
