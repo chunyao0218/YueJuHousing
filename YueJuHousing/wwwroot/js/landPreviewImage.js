0b3df4cde6844162f459d27383d3fea41ad460cb
chunyao0218 <95430616+chunyao0218@users.noreply.github.com>
2024-03-31 13:53:04 +0800

    YJ-013 上傳圖片預覽功能

---
+++ YueJuHousing/wwwroot/js/landPreviewImage.js
@@ -0,0 +1,11 @@
+﻿document.getElementById('chooseImg').addEventListener('change', function () {
+    const file = this.files[0];
+    if (file) {
+        debugger
+        const reader = new FileReader();
+        reader.onload = function () {
+            document.getElementById('imgPreview').src = reader.result;
+        }
+        reader.readAsDataURL(file);
+    }
+});
