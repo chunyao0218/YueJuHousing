ebea5d28aa23b65268c106c494514545e891524d
CHUN-WEI,CHIU <kevin87332000@gmail.com>
2024-09-01 06:08:19 +0800

    YJ-024 -土地簡易篩選完成 -首頁大圖調整完成(差輪播接土地資料圖片)

---
+++ YueJuHousing.Models/ViewModels/LandSearchVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YueJuHousing.Models.ViewModels
+{
+    public class LandSearchVM
+    {
+        public string City { get; set; }
+        public string District { get; set; }
+        public List<decimal> MinPrice { get; set; } = new List<decimal>();
+        public List<decimal> MaxPrice { get; set; } = new List<decimal>();
+        public List<string> Area { get; set; } = new List<string>();
+        public List<string> LandType { get; set; } = new List<string>();
+        public string LandUseZone { get; set; } // "都市土地", "非都市土地", "不限"
+        public List<string> LandUseZoneType { get; set; } = new List<string>(); // "住宅區", "商業區", etc.
+    }
+}
