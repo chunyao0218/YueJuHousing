052c76238f8e15051534f60a80ec3db2741475c5
CHUN-WEI,CHIU <kevin87332000@gmail.com>
2024-09-09 01:42:38 +0800

    YJ-031 -圖片路徑更改 -編輯物件的欄位新增

--- YueJuHousing/Areas/Customer/Controllers/LandController.cs
+++ YueJuHousing/Areas/Customer/Controllers/LandController.cs
@@ -98,7 +98,7 @@ namespace YueJuHousing.Areas.Customer.Controllers
                         {
                             uploadedFile.CopyTo(fileStream);
                         }
-                        imageUrls.Add(@"\images\land\" + fileName);
+                        imageUrls.Add(@"\YueJu\images\land\" + fileName);
                     }
 
                     // 清空之前的图片链接
